d34e684bb735670415112872aecc415f58e4e707
Sharan Varma <sharanvarma0@gmail.com>
2021-07-16 17:58:13 +0530

    initial commit for pychain. A simple commandline blockchain written in python

---
+++ block/block_header.py
@@ -0,0 +1,22 @@
+import datetime
+import ../utilities/blockfunctions
+import ../conf
+
+class Header:
+    def __init__(previous_hash, transactions):
+        self.version = conf.VERSION
+        self.previous_hash = previous_hash
+        self.merkle_root_hash = generatemerklehash(transactions)
+        self.timestamp = datetime.datetime.now()
+
+    def getversion(self):
+        return self.version
+
+    def getmerklehash(self):
+        return self.merkle_root_hash
+
+    def gettime(self):
+        return self.timestamp
+
+
+
---
+++ utilities/blockfunctions.py
@@ -0,0 +1,66 @@
+# Modules used are hashlib: for hashing and deque: for Queue. I did not want to write a new queue when the wheel was already invented
+
+import hashlib
+from collections import deque
+
+# I am choosing the sha256 algorithm here as a global configurable. The intention is it can be changed at any time
+hash_algorithm = hashlib.sha256
+
+''' The Merkle node defines a node in the merkle tree. It generates a node which points to its primitive nodes(None if leaf nodes) along with the combined transaction hash. '''
+class MerkleNode:
+    def __init__(self, transaction_hash, left, right):
+        self.transaction_hash = transaction_hash
+        self.left = left or None
+        self.right = right or None
+
+def generatehash(transaction):
+
+    # Generate the hash for a single transaction
+
+    return hash_algorithm(transaction.encode()).hexdigest()
+
+def createmerklenodes(transactions):
+    
+    # Use the hash values from the transactions to create merkle nodes.
+    merkle_nodes = []
+    for transaction in transactions:
+        merkle_nodes.append(MerkleNode(transaction, None, None))
+    return merkle_nodes
+
+def generatepaddingnode():
+    
+    # The merkle tree operates on pairs. This function generates a padding node to make sure that the nodes are always even numbers. 
+    hash = generatehash('0')
+    return MerkleNode(hash, None, None)
+            
+# Generating the Merkle tree.
+def generatemerklehash(transactions):
+    transaction_hash_array = []
+    for transaction in transactions:
+        transaction_hash_array.append(generatehash(transaction))
+
+    merkle_nodes = deque(createmerklenodes(transaction_hash_array))
+
+    while len(merkle_nodes) > 1:
+        if len(merkle_nodes) % 2 != 0:
+            merkle_nodes.append(generatepaddingnode())
+        length = len(merkle_nodes)
+        for nodeindex in range(0, (length//2)):
+            node1 = merkle_nodes.popleft()
+            node2 = merkle_nodes.popleft()
+            new_hash = generatehash(node1.transaction_hash + node2.transaction_hash)
+            node = MerkleNode(new_hash, node1, node2)
+            merkle_nodes.append(node)
+
+    return merkle_nodes[0].transaction_hash
+
+
+
+
+        
+
+
+
+    
+
+
